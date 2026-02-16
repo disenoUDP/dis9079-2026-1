52ba81a3a506e4123a006f11049f95f00d836f33
aarón montoya-moraga <montoyamoraga@gmail.com>
2026-02-16 15:12:48 -0300

    cambiar a mosquito v5

--- node/script.js
+++ node/script.js
@@ -1,1 +1,14 @@
-console.log("hi");
+let mqtt = require('mqtt');
+
+let client = mqtt.connect('mqtt://localhost:1883', {
+    protocolVersion: 5,
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    clean: true,
+    connectTimeout: 4000,
+    username: 'montoyamoraga',
+    password: 'dis9079',
+    reconnectPeriod: 1000
+});
+
+// mqtt.Client#publish(topic, message, [options], [callback]);
+
