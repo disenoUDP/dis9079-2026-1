335f096b276899c491b69cf8d4d8e494d221eaf0
marlensoto <marlen.soto@mail.udp.cl>
2026-06-22 11:57:41 -0400

    Add files via upload

--- 00-examen/grupo-7/codigos/emisor_planta.ino
+++ 00-examen/grupo-7/codigos/emisor_planta.ino
@@ -8,14 +8,11 @@ const char* CLAVE_RED  = "plantaviva";
 Adafruit_ADS1115 ads;
 WiFiServer servidor(80);
 
-int lecturas[4] = {0, 0, 0, 0};
-int base[4]     = {0, 0, 0, 0};
-int cambio[4]   = {0, 0, 0, 0};
+int lecturas[4]  = {0, 0, 0, 0};
+int base[4]      = {0, 0, 0, 0};
+int cambio[4]    = {0, 0, 0, 0};
 
 String nombres[4] = {"hoja_grande", "hoja_pequena", "tallo", "sustrato"};
-
-unsigned long ultimaLectura = 0;
-const int INTERVALO = 500;
 
 void setup() {
   Serial.begin(9600);
@@ -26,6 +23,7 @@ void setup() {
     while (1);
   }
   ads.setGain(GAIN_SIXTEEN);
+  Serial.println("ADS1115 listo.");
 
   Serial.println("Calibrando... no toques la planta");
   delay(2000);
@@ -36,9 +34,15 @@ void setup() {
       delay(100);
     }
     base[i] = suma / 10;
+    Serial.print("Base canal ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(base[i]);
   }
   Serial.println("Calibrado!");
 
+  Serial.print("Creando red: ");
+  Serial.println(NOMBRE_RED);
   WiFi.beginAP(NOMBRE_RED, CLAVE_RED);
   while (WiFi.status() != WL_AP_LISTENING) {
     delay(500);
@@ -46,34 +50,31 @@ void setup() {
   }
   servidor.begin();
   Serial.println("\nRed lista!");
+  Serial.println("Conectarse a 'planta-sensor' y abrir:");
   Serial.println("http://192.168.4.1/datos");
 }
 
 void loop() {
-  unsigned long ahora = millis();
-  if (ahora - ultimaLectura >= INTERVALO) {
-    ultimaLectura = ahora;
-    for (int i = 0; i < 4; i++) {
-      lecturas[i] = ads.readADC_SingleEnded(i);
-      cambio[i]   = lecturas[i] - base[i];
-    }
-    for (int i = 0; i < 4; i++) {
-      Serial.print(nombres[i]);
-      Serial.print(": ");
-      Serial.print(lecturas[i]);
-      Serial.print(" (");
-      if (cambio[i] >= 0) Serial.print("+");
-      Serial.print(cambio[i]);
-      Serial.println(")");
-    }
-    Serial.println("---");
+  for (int i = 0; i < 4; i++) {
+    lecturas[i] = ads.readADC_SingleEnded(i);
+    cambio[i]   = lecturas[i] - base[i];
   }
+
+  for (int i = 0; i < 4; i++) {
+    Serial.print(nombres[i]);
+    Serial.print(": ");
+    Serial.print(lecturas[i]);
+    Serial.print(" (");
+    if (cambio[i] >= 0) Serial.print("+");
+    Serial.print(cambio[i]);
+    Serial.println(")");
+  }
+  Serial.println("---");
 
   WiFiClient cliente = servidor.available();
   if (cliente) {
     String peticion = "";
-    unsigned long inicio = millis();
-    while (cliente.connected() && millis() - inicio < 1000) {
+    while (cliente.connected()) {
       if (cliente.available()) {
         char c = cliente.read();
         peticion += c;
@@ -95,8 +96,22 @@ void loop() {
       cliente.println("Access-Control-Allow-Origin: *");
       cliente.println("Connection: close");
       cliente.println();
-      cliente.print(json);
+      cliente.println(json);
+
+    } else if (peticion.indexOf("GET / ") >= 0) {
+      cliente.println("HTTP/1.1 200 OK");
+      cliente.println("Content-Type: text/html");
+      cliente.println("Connection: close");
+      cliente.println();
+      cliente.println("<html><body style='background:#000;color:#0f0;font-family:monospace;padding:2rem'>");
+      cliente.println("<h2>La Voz de las Plantas</h2>");
+      cliente.println("<p>Abre <a href='/datos' style='color:#0f0'>/datos</a> para ver los valores</p>");
+      cliente.println("</body></html>");
     }
+
+    delay(10);
     cliente.stop();
   }
+
+  delay(50);
 }
